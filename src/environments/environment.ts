084e2af360d765b2699b97ce3123a98d9f63c20b
Hiệp Phạm Văn <kaissken@outlook.com>
2023-07-17 11:09:31 +0700

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+// The list of file replacements can be found in `angular.json`.
+export const environment = {
+    production: false,
+    apiUrl: 'https://localhost:5001/api/',
+    hubUrl: 'https://localhost:5001/hubs/'
+};
