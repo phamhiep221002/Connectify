52160ada270ca464801e5f7138c61122d3806a0b
Hiệp Phạm Văn <kaissken@outlook.com>
2023-07-17 11:11:28 +0700

    Update enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'api/',
-    hubUrl: 'hubs/'
+    apiUrl: 'https://localhost:5001/api/',
+    hubUrl: 'https://localhost:5001/hubs/'
 };
 
