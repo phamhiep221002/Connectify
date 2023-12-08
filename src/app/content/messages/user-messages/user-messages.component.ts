28d8a05554ad7e063189faa9c452f032cdb9cff1
ilehuukhuong <ilehuukhuong@gmail.com>
2023-12-09 00:17:49 +0700

    Check Deploy

--- src/app/content/messages/user-messages/user-messages.component.ts
+++ src/app/content/messages/user-messages/user-messages.component.ts
@@ -346,14 +346,23 @@ export class UserMessagesComponent implements OnInit, OnDestroy {
   handleNewMessages(): void {
     setTimeout(() => {
       const currentScrollTop = this.myScrollContainer?.nativeElement.scrollTop;
+      console.log(currentScrollTop);
       const newScrollHeight = this.myScrollContainer?.nativeElement.scrollHeight;
+      console.log(newScrollHeight);
   
       // Nếu đang không tải tin nhắn cũ, cuộn xuống cuối
       if (!this.loadingOldMessages) {
+        console.log('không tải tin nhắn cũ');
+        console.log(newScrollHeight);
         this.myScrollContainer!.nativeElement.scrollTop = newScrollHeight;
       } else {
         // Nếu đang tải tin nhắn cũ, cập nhật vị trí cuộn để giữ nguyên vị trí hiện tại của người dùng
         this.myScrollContainer!.nativeElement.scrollTop = newScrollHeight - this.currentScrollHeight + currentScrollTop;
+        console.log('tải tin nhắn cũ');
+        console.log(newScrollHeight );
+        console.log(this.currentScrollHeight);
+        console.log(currentScrollTop);
+        console.log(newScrollHeight - this.currentScrollHeight + currentScrollTop);
       }
     });
   }
